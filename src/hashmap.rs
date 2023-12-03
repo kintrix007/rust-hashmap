562e38b083de45e3aae6e2e66e2a9680c06da5c0
kintrix007 <boldi.koller@proton.me>
2023-12-03 10:24:24 +0100

    feat: Implemented inserting and getting

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -38,7 +38,7 @@ impl HashMap<String, i32> {
         }
     }
 
-    pub fn set(&mut self, key: String, value: i32) {
+    fn get_mut_finger(&mut self, key: &String) -> &mut Option<Box<HashMapItem<String, i32>>> {
         let bucket_count = self.buckets.len();
         let idx = (self.hash)(&key) % bucket_count;
 
@@ -46,15 +46,51 @@ impl HashMap<String, i32> {
         loop {
             match finger {
                 None => break,
-                Some(x) if x.key == key => break,
+                Some(x) if x.key == *key => break,
                 Some(x) => finger = &mut x.next,
             };
         }
 
+        finger
+    }
+
+    // TODO: Figure out how to take `$self` instead of `&mut self` without
+    // copying over the whole body
+    pub fn get(&mut self, key: &String) -> Option<&i32> {
+        self.get_mut_finger(key).as_ref().map(|x| &x.value)
+    }
+    
+    pub fn get_mut(&mut self, key: &String) -> Option<&mut i32> {
+        self.get_mut_finger(key).as_mut().map(|x| &mut x.value)
+    }
+
+    pub fn set(&mut self, key: String, value: i32) {
+        let finger = self.get_mut_finger(&key);
+
         let value = match finger {
             None => HashMapItem::from(key, value, None),
-            Some(x) => HashMapItem::from(key, value, x.next.to_owned()),
+            Some(x) => HashMapItem::from(key, value, x.next.take()),
         };
         *finger = Some(Box::from(value));
+    }
+
+    pub fn remove(&mut self, key: &String) -> Option<i32> {
+        let bucket_count = self.buckets.len();
+        let idx = (self.hash)(&key) % bucket_count;
+
+        let mut finger = &mut self.buckets[idx];
+        let mut prev = &finger;
+        loop {
+            match finger {
+                None => break,
+                Some(x) if x.key == *key => break,
+                Some(x) => {
+                    finger = &mut x.next;
+                    prev = &finger;
+                }
+            };
+        }
+
+        todo!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod hashmap;
 
 fn main() {
-    println!("Hello, world!");
+    let mut map = hashmap::HashMap::new(3);
+    map.set(String::from("Foo"), 1);
+    map.set(String::from("Bar"), 2);
+    map.set(String::from("Baz"), 3);
+    println!("{:?}", map);
+
+    println!("{:?}", map.get(&String::from("Foo")));
+    println!("{:?}", map.get(&String::from("foo")));
+    println!("{:?}", map.get(&String::from("Bar")));
 }
