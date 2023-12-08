48a7c892655aaa16d1ca196d83329fd27ad10f8f
kintrix007 <boldi.koller@proton.me>
2023-12-08 19:22:14 +0100

    Make the key simply have to implement Hash, not specifically be a string

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -1,46 +1,51 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct HashMapItem<K, T> {
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug, PartialEq, Clone)]
+struct HashMapItem<K: Clone, V: Clone> {
     key: K,
-    value: T,
-    next: Option<Box<HashMapItem<K, T>>>,
+    value: V,
+    next: Option<Box<HashMapItem<K, V>>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct HashMap<K, V> {
+#[derive(Debug)]
+pub struct HashMap<K: PartialEq + Hash + Clone, V: Clone> {
     size: usize,
     buckets: Box<[Option<Box<HashMapItem<K, V>>>]>,
-    hash: fn(&K) -> usize,
 }
 
-fn default_hash(x: &String) -> usize {
-    x.chars()
-        .map(|x| x as usize)
-        .reduce(|acc, x| acc * 31 + x)
-        .unwrap_or(0)
+impl<K: PartialEq + Hash + Clone, V: PartialEq + Clone> PartialEq for HashMap<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        *self.items() == *other.items()
+    }
 }
 
-impl HashMapItem<String, i32> {
-    pub fn from(
-        key: String,
-        value: i32,
-        next: Option<Box<HashMapItem<String, i32>>>,
-    ) -> HashMapItem<String, i32> {
+impl<K: Clone, V: Clone> HashMapItem<K, V> {
+    pub fn from(key: K, value: V, next: Option<Box<HashMapItem<K, V>>>) -> HashMapItem<K, V> {
         HashMapItem { key, value, next }
     }
 }
 
-impl HashMap<String, i32> {
-    pub fn new(size: usize) -> HashMap<String, i32> {
+impl<K: PartialEq + Hash + Clone, V: Clone> HashMap<K, V> {
+    pub fn new(size: usize) -> HashMap<K, V> {
         HashMap {
             size: 0,
             buckets: vec![None; size].into_boxed_slice(),
-            hash: default_hash,
         }
     }
 
-    fn get_mut_finger(&mut self, key: &String) -> &mut Option<Box<HashMapItem<String, i32>>> {
-        let bucket_count = self.buckets.len();
-        let idx = (self.hash)(&key) % bucket_count;
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    fn hash(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        hasher.finish() as usize
+    }
+
+    fn get_mut_finger(&mut self, key: &K) -> &mut Option<Box<HashMapItem<K, V>>> {
+        let idx = self.hash(key) % self.buckets.len();
 
         let mut finger = &mut self.buckets[idx];
         loop {
@@ -54,43 +59,76 @@ impl HashMap<String, i32> {
         finger
     }
 
-    // TODO: Figure out how to take `$self` instead of `&mut self` without
+    // TODO: Figure out how to take `&self` instead of `&mut self` without
     // copying over the whole body
-    pub fn get(&mut self, key: &String) -> Option<&i32> {
+    pub fn get(&mut self, key: &K) -> Option<&V> {
         self.get_mut_finger(key).as_ref().map(|x| &x.value)
     }
-    
-    pub fn get_mut(&mut self, key: &String) -> Option<&mut i32> {
+
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
         self.get_mut_finger(key).as_mut().map(|x| &mut x.value)
     }
 
-    pub fn set(&mut self, key: String, value: i32) {
-        let finger = self.get_mut_finger(&key);
+    pub fn set(&mut self, key: &K, value: V) {
+        let finger = self.get_mut_finger(key);
 
-        let value = match finger {
-            None => HashMapItem::from(key, value, None),
-            Some(x) => HashMapItem::from(key, value, x.next.take()),
+        match finger {
+            None => {
+                *finger = Some(Box::from(HashMapItem::from(key.clone(), value, None)));
+                self.size += 1;
+            }
+            Some(x) => x.value = value,
         };
-        *finger = Some(Box::from(value));
     }
 
-    pub fn remove(&mut self, key: &String) -> Option<i32> {
+    pub fn remove(&mut self, key: &K) -> Option<V> {
         let bucket_count = self.buckets.len();
-        let idx = (self.hash)(&key) % bucket_count;
+        let idx = self.hash(key) % bucket_count;
 
         let mut finger = &mut self.buckets[idx];
-        let mut prev = &finger;
-        loop {
-            match finger {
-                None => break,
-                Some(x) if x.key == *key => break,
-                Some(x) => {
-                    finger = &mut x.next;
-                    prev = &finger;
-                }
-            };
+        if finger.as_ref().is_some_and(|x| x.key == *key) {
+            // Is there any way to get rid of this cloning?
+            let val = finger.as_ref().map(|x| x.value.to_owned());
+            *finger = finger.as_mut().and_then(|x| x.next.take());
+            self.size -= 1;
+            return val;
         }
 
+        loop {
+            let is_next_matching = finger
+                .as_ref()
+                .is_some_and(|x| x.next.as_ref().is_some_and(|x| x.key == *key));
+            if is_next_matching {
+                break;
+            }
+
+            match finger {
+                None => break,
+                Some(x) => finger = &mut x.next,
+            }
+        }
+
+        // Is there any way to get rid of this cloning?
+        let val = finger.as_ref().map(|x| x.value.to_owned());
+
+        match finger {
+            None => {}
+            Some(x) => {
+                let next = x.next.as_mut().and_then(|y| y.next.take());
+                x.next = next;
+                self.size -= 1;
+            }
+        };
+
+        val
+    }
+
+    // Instead return an iterator... That would make much more sense
+    pub fn items(&self) -> Vec<(&K, &V)> {
+        todo!();
+    }
+
+    pub fn rehash(&mut self) {
         todo!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ mod hashmap;
 
 fn main() {
     let mut map = hashmap::HashMap::new(3);
-    map.set(String::from("Foo"), 1);
-    map.set(String::from("Bar"), 2);
-    map.set(String::from("Baz"), 3);
+    map.set(&String::from("foo"), 1);
+    map.set(&String::from("bar"), 2);
+    map.set(&String::from("baz"), 3);
+    map.set(&String::from("baz"), 4);
     println!("{:?}", map);
 
     println!("{:?}", map.get(&String::from("Foo")));
     println!("{:?}", map.get(&String::from("foo")));
     println!("{:?}", map.get(&String::from("Bar")));
+
+    map.remove(&String::from("foo"));
+    println!("{:?}", map);
+    map.remove(&String::from("bar"));
+    println!("{:?}", map);
+    map.remove(&String::from("baz"));
+    println!("{:?}", map);
 }
