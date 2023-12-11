13422ec30d8e1b714fb6e8d3e9a2ea1159394b9c
kintrix007 <kintrix007@proton.me>
2023-12-11 21:24:10 +0100

    Update main.rs why not... It's not like it actually plays a role, though

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,16 @@ fn main() {
     map.set(&String::from("bar"), 2);
     map.set(&String::from("baz"), 3);
     map.set(&String::from("baz"), 4);
+    map.set(&String::from("Hello!"), 42);
     println!("{:?}", map);
 
     println!("{:?}", map.get(&String::from("Foo")));
     println!("{:?}", map.get(&String::from("foo")));
     println!("{:?}", map.get(&String::from("Bar")));
+
+    for (k, v) in map.iter() {
+        println!("{}: {}", k, v);
+    }
 
     map.remove(&String::from("foo"));
     println!("{:?}", map);
